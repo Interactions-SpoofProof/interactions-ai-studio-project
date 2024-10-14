3dfbf7cf360b5a9476df7c719adbed30a1cfaef1
Marilyn Jin <marilynj468@gmail.com>
2024-10-14 15:17:27 -0400

    updated pyttsx3 script file

--- pyttsx3_synthetic_audio.py
+++ pyttsx3_synthetic_audio.py
@@ -15,21 +15,19 @@ rate = engine.getProperty('rate')
 data = pd.read_csv('train.tsv', sep='\t')
 sentences = data['sentence']
 
-# using 100 sentences from common voice
-sentences = sentences[3100: 3105]
-#sentences = sentences[3000:3100]
+# using 500 sentences from common voice
+sentences = sentences[60000: 60500]
 
 # Show the first few sentences in the slice
 print(sentences.head())
 
-index = 100
 # loop to create each audio file
 for i, j in enumerate(sentences):
   if j != '':
     engine.say(j)
-    file = 'pyttsx3_files/pyttsx3_synth_' + str(index) + '.mp3'
+    file = 'pyttsx3_files/pyttsx3_synth_' + str(i) + '.mp3'
     engine.save_to_file(j, file)
-    index +=1
+    # index +=1
 
 engine.runAndWait()
 
@@ -41,4 +39,33 @@ for voice in voices:
    print(f'index-> {index} -- {voice.name}')
    index +=1
 engine.runAndWait()
+'''
+
+'''
+# looping through 7 of the voices
+voices = engine.getProperty('voices')
+voice_indexes = [0, 7, 10, 17, 28, 33, 37]
+
+for index in voice_indexes:
+   print(f'index-> {index} -- {voices[index].name}')
+   
+   engine.setProperty('voice', voices[index].id)
+   engine.say("Hello, this is voice number" + str(index)+ " " + voices[index].name +  " I am not a robot.")
+   index +=1
+
+engine.runAndWait()
+'''
+
+# looping through all voices
+'''
+index = 0
+for voice in voices:
+   print(f'index-> {index} -- {voice.name}')
+   
+   engine.setProperty('voice', voices[index].id)
+   engine.say("Hello, this is voice number" + str(index)+ " " + voice.name +  " I am not a robot.")
+   index +=1
+
+engine.runAndWait()
+
 '''
